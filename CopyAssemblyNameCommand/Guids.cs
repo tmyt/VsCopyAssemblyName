4eb2efae32134535de714aedfaccc79e2877e458
yutaka <shigure@refy.net>
2015-02-16 17:32:10 +0900

    それっぽく動いてそう

---
+++ CopyAssemblyNameCommand/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace tmyt.CopyAssemblyNameCommand
+{
+    static class GuidList
+    {
+        public const string guidCopyAssemblyNameCommandPkgString = "c8fd9856-9afe-42fc-a293-d1072c94e770";
+        public const string guidCopyAssemblyNameCommandCmdSetString = "4e78a8d1-3732-4dab-879c-c5da77c444ee";
+
+        public static readonly Guid guidCopyAssemblyNameCommandCmdSet = new Guid(guidCopyAssemblyNameCommandCmdSetString);
+    };
+}
---
+++ CopyAssemblyNameCommand/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace tmyt.CopyAssemblyNameCommand
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidCopyAssemblyName =        0x100;
+
+
+    };
+}
